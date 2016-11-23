e3d6a4dd2afea9360b863164da5f106d90c833e8
danielisaacgeslin <danielisaacgeslin@gmail.com>
2016-11-23 10:22:52 -0300

    update

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/angular-ui-router/index.d.ts" />
-/// <reference path="globals/angular/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="modules/debug/index.d.ts" />
