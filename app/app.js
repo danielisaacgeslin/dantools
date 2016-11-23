6226d7abee10ab0f6d5cd57b843a5ecc1b3e9ec6
danielisaacgeslin <danielisaacgeslin@gmail.com>
2016-11-23 10:38:58 -0300

    update

--- app/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-require('./core/modules/app.module');
-require('./core/modules/app.core.module');
-require('./home/modules/app.main.module');
-require('./home/controllers/main.controller');
-require('./home/controllers/second.controller');
